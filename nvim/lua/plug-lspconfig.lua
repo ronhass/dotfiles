94f937ba53dab3707b53c4a44903a8a0aeda44cd
Ron Hass <ronhass37@gmail.com>
2023-10-28 22:39:41 +0300

    Replace lua lsp server

--- nvim/lua/plug-lspconfig.lua
+++ nvim/lua/plug-lspconfig.lua
@@ -52,12 +52,26 @@ require('lspconfig')['rust_analyzer'].setup({
     -- }
 })
 
-require('lspconfig')['sumneko_lua'].setup{
+require'lspconfig'.lua_ls.setup {
     on_attach = on_attach,
     settings = {
         Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+            },
             diagnostics = {
-                globals = {"vim"},
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim'},
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
             },
         },
     },
