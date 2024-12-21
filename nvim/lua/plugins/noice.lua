8e8efdfc735fca0277ce3e90310c58ae7f6dd949
Ron Hass <ronhass37@gmail.com>
2024-12-21 23:14:56 +0200

    Hide "written" message

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -18,6 +18,16 @@ return {
                 long_message_to_split = true, -- long messages will be sent to a split
                 lsp_doc_border = true, -- add a border to hover docs and signature help
             },
+            routes = {
+                {
+                    filter = {
+                        event = "msg_show",
+                        kind = "",
+                        find = "written",
+                    },
+                    opts = { skip = true },
+                },
+            },
         },
         dependencies = {
             "MunifTanjim/nui.nvim",
@@ -25,6 +35,6 @@ return {
         },
         keys = {
             { "<leader>q", "<cmd>:Noice dismiss<cr>", "Dismiss alerts" },
-        }
+        },
     },
 }
